3ef4f17459f8f29a1bf73dae10a689eb4dfef829
Nayan Raj <nayanraj200506@gmail.com>
2026-08-02 23:27:50 +0530

    Add files via upload

---
+++ Password_generator.py
@@ -0,0 +1,8 @@
+import string
+import random
+
+length = int(input("Enter the desired password length: "))
+characters = string.ascii_letters + string.digits + string.punctuation
+password = ''.join(random.choice(characters) for _ in range(length))
+
+print("Generated Password:", password)
